87da43edc5d22471fa2f87a070c61e1a795c26f5
AmuroEita <1071307515@qq.com>
2025-02-09 22:20:45 +0800

    update

--- neurips23/concurrent/diskann/diskann.py
+++ neurips23/concurrent/diskann/diskann.py
@@ -1,15 +1,13 @@
 import numpy as np
 from numpy import typing as npt
 
-from neurips23.congestion.base import BaseCongestionDropANN
-from neurips23.streaming.diskann.diskann import  diskann as diskann_streaming
-import torch
+from neurips23.concurrent.base import BaseConcurrentANN
+from neurips23.concurrent.diskann.diskann import diskann as diskann_concurrent
 
-class diskann(BaseCongestionDropANN):
+class diskann(BaseConcurrentANN):
     def __init__(self, metric, index_params):
-        super().__init__([diskann_streaming(metric, index_params)], metric, index_params)
+        super().__init__([diskann_concurrent(metric, index_params)], metric, index_params)
         self.metric = metric
-        #self.indexkey=self.workers[0].my_index_algo.indexkey
         self.name = self.workers[0].my_index_algo.index_name()
 
     def set_query_arguments(self, query_args):
